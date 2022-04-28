adff19ef985c49d24e1b4022292071af9a79db3c
Lucas Omar Moreno <ing.lucasmoreno@gmail.com>
2022-04-28 19:29:07 -0300

    Mapa

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
+import { Map, tileLayer } from 'leaflet';
 import gsap from 'gsap';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: [
-  ]
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
@@ -16,5 +16,14 @@ export class HomeComponent implements OnInit {
     gsap.from('.gsap-contenido', { y:100, opacity: 0, duration: .2 });
     this.dataService.ubicacionActual = 'Dashboard - Inicio';
   }
+
+  ngAfterViewInit(): void {
+    var map = new Map('map').setView([51.505, -0.09], 13);  
+    tileLayer('https://{s}.tile.openstreetmap.de/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+  }
+
   
 }
